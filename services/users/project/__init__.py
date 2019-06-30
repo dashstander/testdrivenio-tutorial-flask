3136759c244026ca879c745ebe87f599b3ca79b4
Dashiell Stander <dstander@zuora.com>
2019-06-30 14:11:28 -0700

    add react client

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,11 +1,14 @@
 # services/users/project/__init__.py
 import os
 from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()
-
+cors = CORS()
+toolbar = DebugToolbarExtension()
 
 def create_app(script_info=None):
     app = Flask(__name__)
@@ -15,6 +18,8 @@ def create_app(script_info=None):
     app.config.from_object(app_settings)
 
     db.init_app(app)
+    cors.init_app(app)
+    toolbar.init_app(app)
 
     from project.api.users import users_blueprint
     app.register_blueprint(users_blueprint)
